7d4e3822818146e38e91cfc0eaa6d4e92cd143c0
Kopyn <mackopin@gmail.com>
2025-12-23 15:21:26 +0100

    created spring boot projects for all of the services

---
+++ account-info-service/src/test/java/com/kopyn/cqrs/account_info_service/AccountInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopyn.cqrs.account_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ transaction-history-service/src/main/java/com/kopyn/cqrs/transaction_history_service/TransactionHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kopyn.cqrs.transaction_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionHistoryServiceApplication.class, args);
+	}
+
+}
